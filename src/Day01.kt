1dd73ed09d07e18dc1cbf93844e46052c791102c
PP6464 <panthhp@outlook.com>
2024-12-01 11:33:08 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,19 @@
+import kotlin.math.*
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+	fun part1(input : List<String>) : Int {
+		val list1 = input.map { it.split("   ")[0].toInt() }.sorted()
+		val list2 = input.map { it.split("   ")[1].toInt() }.sorted()
+		return list1.zip(list2).sumOf { (it.second - it.first).absoluteValue }
+	}
+	
+	fun part2(input : List<String>) : Int {
+		val list1 = input.map { it.split("   ")[0].toInt() }.sorted()
+		val list2 = input.map { it.split("   ")[1].toInt() }.sorted()
+		return list1.sumOf { it * list2.count { x -> x == it } }
+	}
+	
+	val input = readInput("Day01")
+	part1(input).println()
+	part2(input).println()
 }

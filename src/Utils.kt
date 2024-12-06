eb1fa006abe1be5d7ee775e1322cbd8d72dd230a
PP6464 <panthhp@outlook.com>
2024-12-06 13:15:31 +0000

    Move utility stuff to one file

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,5 +1,6 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.math.*
 
 /**
  * Reads lines from the given input txt file.
@@ -10,3 +11,95 @@ fun readInput(name : String) = Path("src/inputs/$name.txt").readText().trim().li
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+enum class Directions {
+	// They're ordered this way so that the sum of opposite direction's ordinals = 7
+	UP,
+	RIGHT,
+	UP_RIGHT,
+	UP_LEFT,
+	DOWN_RIGHT,
+	DOWN_LEFT,
+	LEFT,
+	DOWN;
+	
+	private fun newCoordinates(x : Int, y : Int) : Pair<Int, Int> {
+		return when (this) {
+			UP -> x to y - 1
+			DOWN -> x to y + 1
+			RIGHT -> x + 1 to y
+			LEFT -> x - 1 to y
+			UP_RIGHT -> x + 1 to y - 1
+			DOWN_RIGHT -> x + 1 to y + 1
+			DOWN_LEFT -> x - 1 to y + 1
+			UP_LEFT -> x - 1 to y - 1
+		}
+	}
+	
+	fun newCoordinates(coords : Pair<Int, Int>) : Pair<Int, Int> {
+		return newCoordinates(coords.first, coords.second)
+	}
+	
+	fun isOpposite(other: Directions) : Boolean {
+		return ordinal + other.ordinal == 7
+	}
+}
+
+// Queries a list of rows at column x (0-indexed) and row y (0-indexed where top = 0)
+fun List<String>.gridQuery(coords : Pair<Int, Int>) : Char = get(coords.second)[coords.first]
+
+fun List<String>.gridIndicesOf(c : Char) : List<Pair<Int, Int>> {
+	val gridIndices = mutableListOf<Pair<Int, Int>>()
+	
+	for (y in indices) {
+		for (x in 0..<get(y).length) {
+			if (get(y)[x] == c) {
+				gridIndices.add(x to y)
+			}
+		}
+	}
+	
+	return gridIndices
+}
+
+// Steps are like Directions but for when you can't go diagonally
+enum class Steps {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT;
+	
+	fun nextCoord(coord : Pair<Int, Int>) : Pair<Int, Int> {
+		return when (this) {
+			UP -> coord.first to coord.second - 1
+			DOWN -> coord.first to coord.second + 1
+			LEFT -> coord.first - 1 to coord.second
+			RIGHT -> coord.first + 1 to coord.second
+		}
+	}
+	
+	fun turnRight() : Steps {
+		return when (this) {
+			UP -> RIGHT
+			DOWN -> LEFT
+			LEFT -> UP
+			RIGHT -> DOWN
+		}
+	}
+	
+	companion object {
+		fun dirnBetween(a: Pair<Int, Int>, b: Pair<Int, Int>) : Steps {
+			return if ((a.first - b.first).absoluteValue + (a.second - b.second).absoluteValue != 1) {
+				throw Exception("The points must be adjacent")
+			} else if (a.second - b.second == 1) {
+				UP
+			} else if (b.second - a.second == 1) {
+				DOWN
+			} else if (b.first - a.first == 1) {
+				RIGHT
+			} else {
+				LEFT
+			}
+		}
+	}
+}
